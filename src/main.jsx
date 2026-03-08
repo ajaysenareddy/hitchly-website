51ff6e03235ae002b77b35f750a1f02224d0f143
ajaysenareddy <ajaysenareddy@gmail.com>
2026-03-08 11:57:57 +0530

    Fix Vite base URL for custom domain hitchly.in

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import BenefitsOfCarpooling from './pages/BenefitsOfCarpooling.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/car-pool-hyderabad" element={<CarpoolHyderabad />} />
